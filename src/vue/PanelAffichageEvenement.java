8255b2270a00a241fb0b06f87a22d6106be91d97
FocSon <ifocson@gmail.com>
2020-06-10 19:47:30 +0200

    Debut de mise en fonctionnement de la classe

--- src/vue/PanelAffichageEvenement.java
+++ src/vue/PanelAffichageEvenement.java
@@ -1,8 +1,85 @@
 package vue;
 
-import javax.swing.JPanel;
+import modele.Chronologie;
+import modele.Evenement;
 
-public class PanelAffichageEvenement extends JPanel {
-    public PanelAffichageEvenement() {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Iterator;
+
+public class PanelAffichageEvenement extends JPanel implements ActionListener {
+    private JButton precedent;
+    private JButton suivant;
+    private Evenement eventEnCours;
+    private Chronologie frise;
+    private JLabel labelDesc;
+    private JLabel labelImage;
+
+    public PanelAffichageEvenement(Chronologie parFrise) {
+        setLayout(new FlowLayout());
+
+        frise = parFrise;
+
+        Iterator<Evenement> iterateur = frise.getListeEvt().iterator();
+
+        labelImage = new JLabel();
+        labelDesc = new JLabel();
+        reinitEvent(iterateur.next());
+
+        precedent = new JButton("<");
+        suivant = new JButton(">");
+
+        precedent.addActionListener(this);
+        suivant.addActionListener(this);
+
+        add(precedent);
+        add(labelImage);
+        add(labelDesc);
+        add(suivant);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent event) {
+        if(event.getSource()==precedent){
+            Iterator<Evenement> iterateurCourant = frise.getListeEvt().iterator();
+
+            for(Iterator<Evenement> iterateurPrevision = frise.getListeEvt().iterator(); iterateurPrevision.hasNext() && iterateurPrevision.next()!=eventEnCours;)
+                iterateurCourant=iterateurPrevision;
+
+            reinitEvent((Evenement) iterateurCourant);
+        }
+
+        else if(event.getSource()==suivant){
+            Iterator<Evenement> iterateur = frise.getListeEvt().iterator();
+            while(iterateur.hasNext() && iterateur!=eventEnCours){
+                iterateur.next();
+            }
+            reinitEvent(iterateur.next());
+        }
+    }
+
+    private void reinitEvent(Evenement event){
+        eventEnCours=event;
+
+        labelImage.setIcon(event.getImage());
+
+        String description;
+        if(event.getTexteDescriptif().split("\n").length > 1) {
+            description = "<html>";
+            String[] temp = event.getTexteDescriptif().split("\n");
+            for (int indice = 0; indice < temp.length - 1; indice++)
+                description += temp[indice] + "<br>";
+            description += temp[temp.length - 1] + "</html>";
+        }
+
+        else
+            description = event.getTexteDescriptif();
+
+        labelDesc.setText(description);
+
+        validate();
+        repaint();
     }
 }
