6104cb524278ebac40292f5b9d293d1e4c517208
FocSon <ifocson@gmail.com>
2020-06-10 19:47:10 +0200

    Initialisation de PanelAffichage

--- src/vue/PanelAffichage.java
+++ src/vue/PanelAffichage.java
@@ -1,8 +1,21 @@
 package vue;
 
-import javax.swing.JPanel;
+import modele.Chronologie;
+
+import javax.swing.*;
+import java.awt.*;
 
 public class PanelAffichage extends JPanel {
-    public PanelAffichage() {
+
+    public PanelAffichage(Chronologie frise) {
+        setLayout(new BorderLayout());
+
+        add((new JLabel(frise.getIntitule())), BorderLayout.NORTH);
+
+        PanelAffichageEvenement affichageEvent = new PanelAffichageEvenement(frise);
+        add(affichageEvent, BorderLayout.CENTER);
+
+        PanelAffichageTable affichageTable = new PanelAffichageTable();
+        add(affichageTable, BorderLayout.SOUTH);
     }
 }
