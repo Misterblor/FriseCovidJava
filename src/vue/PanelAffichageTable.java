397564a1c19e88384c8c6ec5e346106e3426a0dc
FocSon <ifocson@gmail.com>
2020-06-11 01:11:54 +0200

    Complétion du constructeur

--- src/vue/PanelAffichageTable.java
+++ src/vue/PanelAffichageTable.java
@@ -1,8 +1,18 @@
 package vue;
 
-import javax.swing.JPanel;
+import modele.Chronologie;
+import modele.ModeleTableFrise;
+
+import javax.swing.*;
+import java.awt.*;
 
 public class PanelAffichageTable extends JPanel {
-    public PanelAffichageTable() {
+    public PanelAffichageTable(Chronologie frise) {
+        JTable tableFrise = new JTable();
+        tableFrise.setModel(new ModeleTableFrise(frise));
+
+        JScrollPane scrollPane = new JScrollPane(tableFrise,ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER,ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        scrollPane.setPreferredSize(new Dimension(650,500));
+        add(scrollPane);
     }
 }
