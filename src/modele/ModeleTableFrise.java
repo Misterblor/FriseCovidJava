1959d18d5f3ce23057b4e6a648a7069379f37f4c
FocSon <ifocson@gmail.com>
2020-06-11 11:44:57 +0200

    adaptation à la metode nbJourEntre (voir pour ajouter nbAnneeEntre et nbMoisEntre)

--- src/modele/ModeleTableFrise.java
+++ src/modele/ModeleTableFrise.java
@@ -5,8 +5,9 @@ import javax.swing.table.DefaultTableModel;
 public class ModeleTableFrise extends DefaultTableModel {
     public ModeleTableFrise(Chronologie frise) {
         setRowCount(4);
-        int nbColonne = nbCol(frise);
+        int nbColonne = nbCol(frise.getDateDebut(), frise.getDateFin(), frise.getPeriode());
         setColumnCount(nbColonne);
+        System.out.println(nbColonne);
 
         setColumnIdentifiers(getIdentifieur(frise, nbColonne));
     }
@@ -14,16 +15,22 @@ public class ModeleTableFrise extends DefaultTableModel {
     private String[] getIdentifieur(Chronologie frise, int nbColonne) {
         String [] identifieur = new String[nbColonne];
 
-        Date dateTemp = new Date(frise.getDateDebut().getJour(), frise.getDateDebut().getMois(), frise.getDateDebut().getAnnee());
-
-        if(frise.getPeriode()!=0)
-            dateTemp = dateTemp.datePremierJourSemaine();
+        Date dateTemp;
+        if(frise.getPeriode()==0 || frise.getPeriode()==1) {
+            dateTemp = new Date(frise.getDateDebut().getJour(), frise.getDateDebut().getMois(), frise.getDateDebut().getAnnee());
+            if (frise.getPeriode() == 1)
+                dateTemp = dateTemp.datePremierJourSemaine();
+        }
+        else if(frise.getPeriode()==2)
+            dateTemp = new Date(1, frise.getDateDebut().getMois(), frise.getDateDebut().getAnnee());
+        else
+            dateTemp = new Date(1, 1, frise.getDateDebut().getAnnee());
 
         identifieur[0]=dateTemp.toString();
 
         for(int i=1; i<nbColonne; i++){
-            dateTemp = faireAvancerDate(dateTemp, frise.getPeriode());
-            if(i%3==0)
+            dateTemp = Date.faireAvancerDate(dateTemp, frise.getPeriode());
+            if(i%3==0 || i+1==nbColonne)
                 identifieur[i]=dateTemp.toString();
             else
                 identifieur[i]="";
@@ -32,50 +39,24 @@ public class ModeleTableFrise extends DefaultTableModel {
         return identifieur;
     }
 
-    private int nbCol(Chronologie frise){
-        int compteur=0;
+    private int nbCol(Date debut, Date fin, int periode){
+        int nbJourEcart=debut.nbJourEntre(fin);
 
-        Date dateTemp = new Date(frise.getDateDebut().getJour(), frise.getDateDebut().getMois(), frise.getDateDebut().getAnnee());
-
-        if(frise.getPeriode()!=0) {
-            dateTemp = dateTemp.datePremierJourSemaine();
-            compteur=1;
-        }
-
-        while(dateTemp.compareTo(frise.getDateFin())<0){
-            dateTemp = faireAvancerDate(dateTemp, frise.getPeriode());
-            compteur++;
-        }
-
-        return compteur;
-    }
-
-    private Date faireAvancerDate(Date dateTemp, int periode){
         if(periode==0)
-            dateTemp = dateTemp.dateDuLendemain();
-        else if(periode==1) {
-            for (int decompte = 0; decompte < 7; decompte++)
-                dateTemp = dateTemp.dateDuLendemain();
-        }
-        else if(periode==2){
-            dateTemp.setJour(Date.dernierJourDuMois(dateTemp.getMois(), dateTemp.getAnnee()));
-            dateTemp=dateTemp.dateDuLendemain();
-        }
-        else if(periode==3){
-            dateTemp = dateTemp.anneeSuivante();
-        }
-        else if(periode==4){
-            for(int decompte=0; decompte<5; decompte++)
-                dateTemp = dateTemp.anneeSuivante();
-        }
-        else if(periode==5){
-            for(int decompte=0; decompte<10; decompte++)
-                dateTemp = dateTemp.anneeSuivante();
-        }
-        else{
-            for(int decompte=0; decompte<100; decompte++)
-                dateTemp = dateTemp.anneeSuivante();
-        }
-        return dateTemp;
+            return nbJourEcart+1;
+        else if(periode==1)
+            return nbJourEcart%7==0 ? nbJourEcart/7 : (nbJourEcart/7)+1;
+        else if(periode==2)
+            return nbJourEcart%30==0 ? nbJourEcart/30 : (nbJourEcart/30)+1;
+        else if(periode==3)
+            return nbJourEcart%365==0 ? nbJourEcart/365 : (nbJourEcart/365)+1;
+        else if(periode==4)
+            return nbJourEcart%1826==0 ? nbJourEcart/1826 : (nbJourEcart/1826)+1;
+        else if(periode==5)
+            return nbJourEcart%3653==0 ? nbJourEcart/3653 : (nbJourEcart/3653)+1;
+        else if(periode==6)
+            return nbJourEcart%3653==0 ? nbJourEcart/3653 : (nbJourEcart/3653)+1;
+
+        return 0;
     }
 }
