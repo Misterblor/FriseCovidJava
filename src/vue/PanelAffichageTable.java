59ebab32cb0b94c99612c33798c594699b2bdb56
FocSon <ifocson@gmail.com>
2020-06-14 21:00:01 +0200

    Table prête à acceuillir des images de 100 par 100 + ajout scrollbar

--- src/vue/PanelAffichageTable.java
+++ src/vue/PanelAffichageTable.java
@@ -10,9 +10,16 @@ public class PanelAffichageTable extends JPanel {
     public PanelAffichageTable(Chronologie frise) {
         JTable tableFrise = new JTable();
         tableFrise.setModel(new ModeleTableFrise(frise));
+        tableFrise.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+        tableFrise.setRowHeight(100);
+
+        for (int i = 0; i < tableFrise.getColumnCount(); i++) {
+            System.out.println(i);
+            tableFrise.getColumnModel().getColumn(i).setPreferredWidth(100);
+        }
 
         JScrollPane scrollPane = new JScrollPane(tableFrise,ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER,ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-        scrollPane.setPreferredSize(new Dimension(650,500));
+        scrollPane.setPreferredSize(new Dimension(650,440));
         add(scrollPane);
     }
 }
