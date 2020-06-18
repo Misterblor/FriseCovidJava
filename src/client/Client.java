53c7008cd282db105db9c0442ba2f1158da116fe
FocSon <ifocson@gmail.com>
2020-06-18 19:47:19 +0200

    Client

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,9 +1,6 @@
 package client;
 
-import modele.Chronologie;
-import modele.Date;
-import modele.Evenement;
-import modele.LectureEcriture;
+import modele.*;
 import vue.FenetreAccueil;
 
 import java.io.File;
@@ -11,19 +8,21 @@ import java.io.File;
 public class Client {
     public static void main(String[] args) {
 
-        Chronologie test1 = new Chronologie("GitHub", new Date(1,1,1900), new Date(5,1,1900), 3, "saves" + File.separator + "test1.ser", "saves" + File.separator + "github.jpg");
-        System.out.print(test1.ajout(new Evenement(new Date(1,1,1900), "test1", 0, "Ceci est un test pour voir si tout foncionne bien\nCa reste à voir...", "saves" + File.separator + "github.jpg")));
+        Chronologie test1 = new Chronologie("GitHub", new Date(1,1,1900), new Date(30,1,1900), 0, "test3.ser", "saves" + File.separator + "github.jpg");
+        System.out.print(test1.ajout(new Evenement(new Date(1,1,1900), "test1", 0, "Ceci est un test pour voir si tout foncionne bien. Cela reste à voir...", "saves" + File.separator + "github.jpg")));
         System.out.print(test1.ajout(new Evenement(new Date(1,1,1900), "test2", 1, "Ceci est un test pour voir si tout foncionne bien\nCa reste à voir...", "saves" + File.separator + "github.jpg")));
         System.out.print(test1.ajout(new Evenement(new Date(2,1,1900), "test3", 0, "Ceci est un test pour voir si tout foncionne bien\nCa restjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjje à voir...", "saves" + File.separator + "github.jpg")));
         System.out.print(test1.ajout(new Evenement(new Date(4,1,1900), "test5", 0, "Ceci est un test pour voir si tout foncionne bien\nCa reste à voir...", "saves" + File.separator + "github.jpg")));
-        System.out.print(test1.ajout(new Evenement(new Date(5,1,1900), "test6", 0, "Ceci est un test pour voir si tout foncionne bien\nCa reste à voir...", "saves" + File.separator + "github.jpg")));
-        LectureEcriture.ecriture(new File(test1.getAdresseFichierSauvegarde()),test1);
+        System.out.print(test1.ajout(new Evenement(new Date(30,1,1900), "test6", 0, "Ceci est un test pour voir si tout foncionne bien\nCa reste à voir...", "saves" + File.separator + "github.jpg")));
 
-        /*
-        Date date1 = new Date(1,6,2000);
-        Date date2 = new Date(3,6,2000);
-        System.out.println(date1.nbJourEntre(date2));
-        */
+        SavesChronologie save = new SavesChronologie();
+        save.add(test1.getAdresseFichierSauvegarde());
+        LectureEcriture.ecriture(new File(test1.getAdresseFichierSauvegarde()),test1);
+/*
+        SavesChronologie save = new SavesChronologie();
+        save.add("test");
+        LectureEcriture.ecriture(SavesChronologie.FILE, save);*/
+
         new FenetreAccueil();
     }
 }
