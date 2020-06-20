94c8934e39d511253a33a4d28e5c6b37bc95a1bb
FocSon <ifocson@gmail.com>
2020-06-20 16:52:27 +0200

    Refonte avec gridbaglayout.

--- src/vue/PanelAffichage.java
+++ src/vue/PanelAffichage.java
@@ -8,18 +8,25 @@ import java.awt.*;
 public class PanelAffichage extends JPanel {
 
     public PanelAffichage(Chronologie frise) {
-        setLayout(new BorderLayout());
+        setLayout(new GridBagLayout());
+        GridBagConstraints contrainte = new GridBagConstraints();
 
         JLabel labelIntitule = new JLabel(frise.getIntitule(), JLabel.CENTER);
         labelIntitule.setFont(new Font("Calibri", Font.BOLD, 20));
-        add(labelIntitule, BorderLayout.NORTH);
-        labelIntitule.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createEmptyBorder(10,0,0,0),labelIntitule.getBorder()));
+        contrainte.gridx=0;
+        contrainte.gridy=0;
+        contrainte.gridwidth=3;
+        contrainte.anchor=GridBagConstraints.CENTER;
+        add(labelIntitule, contrainte);
 
+        contrainte.insets=new Insets(0,0,10,0);
+        contrainte.gridx=1;
+        contrainte.gridy=1;
+        contrainte.gridheight=10;
+        contrainte.gridwidth=1;
+        contrainte.anchor=GridBagConstraints.CENTER;
         PanelAffichageEvenement affichageEvent = new PanelAffichageEvenement(frise);
-        add(affichageEvent, BorderLayout.CENTER);
-
-        PanelAffichageTable affichageTable = new PanelAffichageTable(frise, affichageEvent);
-        add(affichageTable, BorderLayout.SOUTH);
+        add(affichageEvent, contrainte);
 
         JButton precedent = new JButton("<");
         JButton suivant = new JButton(">");
@@ -27,8 +34,8 @@ public class PanelAffichage extends JPanel {
         precedent.addActionListener(affichageEvent);
         suivant.addActionListener(affichageEvent);
 
-        precedent.setPreferredSize(new Dimension(40,115));
-        suivant.setPreferredSize(new Dimension(40,115));
+        precedent.setPreferredSize(new Dimension(20,135));
+        suivant.setPreferredSize(new Dimension(20,135));
 
         precedent.setActionCommand("precedent");
         suivant.setActionCommand("suivant");
@@ -36,11 +43,24 @@ public class PanelAffichage extends JPanel {
         precedent.setBackground(getBackground());
         suivant.setBackground(getBackground());
 
-        precedent.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createEmptyBorder(0,20,0,0),precedent.getBorder()));
-        suivant.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createEmptyBorder(0,0,0,20),suivant.getBorder()));
+        contrainte.gridx=0;
+        contrainte.anchor=GridBagConstraints.LINE_START;
+        contrainte.insets=new Insets(0,20,10,0);
+        add(precedent, contrainte);
 
-        add(precedent, BorderLayout.WEST);
-        add(suivant, BorderLayout.EAST);
+        contrainte.gridx=2;
+        contrainte.anchor=GridBagConstraints.LINE_END;
+        contrainte.insets=new Insets(0,0,10,20);
+        add(suivant, contrainte);
+
+        contrainte.gridwidth=3;
+        contrainte.gridheight=20;
+        contrainte.gridx=0;
+        contrainte.gridy=11;
+        contrainte.anchor=GridBagConstraints.CENTER;
+        contrainte.insets=new Insets(0,0,0,0);
+        PanelAffichageTable affichageTable = new PanelAffichageTable(frise, affichageEvent);
+        add(affichageTable, contrainte);
 
         affichageEvent.setAffichageTable(affichageTable);
     }
