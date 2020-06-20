b3c2b3892eaa000acd2e0caca7d31a312a134399
Pablo <misterblor@gmail.com>
2020-06-20 17:00:21 +0200

    Modification Controleur

--- src/controleur/Controleur.java
+++ src/controleur/Controleur.java
@@ -3,10 +3,8 @@ package controleur;
 import modele.Chronologie;
 import modele.Date;
 import modele.LectureEcriture;
-import vue.FenetreAccueil;
-import vue.PanelAffichage;
-import vue.PanelFormulaireChronologie;
-import vue.PanelFrise;
+import modele.SavesChronologie;
+import vue.*;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -14,9 +12,14 @@ import java.io.File;
 
 public class Controleur implements ActionListener {
     PanelFrise panelFrise;
+    PanelFormulaireChronologie panelFormulaireChronologie;
+    PanelFormulaireEvenement panelFormulaireEvenement;
 
-    public Controleur(PanelFrise parPanelFrise){
+    public Controleur(PanelFrise parPanelFrise, PanelFormulaireChronologie parPanelFormulaireChronologie, PanelFormulaireEvenement parPanelFormulaireEvenement){
     panelFrise = parPanelFrise;
+    panelFormulaireChronologie = parPanelFormulaireChronologie;
+    panelFormulaireChronologie.getButtonAjouter().addActionListener(this);
+    panelFormulaireEvenement = parPanelFormulaireEvenement;
     }
 
     public void actionPerformed(ActionEvent event) {
@@ -26,6 +29,10 @@ public class Controleur implements ActionListener {
 
         else if((event.getActionCommand().split("'"))[0].compareTo("Charger")==0){
             panelFrise.setAffichage(new PanelAffichage((Chronologie) LectureEcriture.lecture(new File(event.getActionCommand().split("'")[1]))));
+        }
+
+        if (event.getSource() == panelFormulaireChronologie.getButtonAjouter()){
+            panelFormulaireChronologie.estValide();
         }
     }
 }
